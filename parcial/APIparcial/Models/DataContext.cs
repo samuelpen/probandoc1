47f6c49bdad9c950b72f8da26c3bec6f52d28546
Samuel Peña <samuel_pena@hotmail.es>
2019-04-06 00:20:50 -0400

    commit con modelo WebAPI

---
+++ parcial/APIparcial/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIparcial.Models
+{
+    public class DataContext:DbContext 
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIparcial.Models.Product> Products { get; set; }
+    }
+}
---
+++ parcial/APIparcial/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace APIparcial.Models
+{
+    public enum UniType
+    {
+        Botella,
+        Litro,
+        Metro
+    }
+
+    public enum StatusType {
+        Activo,
+        Inactivo
+    }
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        [Required]
+        public UniType Unit { get; set; }
+        public StatusType Status { get; set; }
+
+    }
+}
